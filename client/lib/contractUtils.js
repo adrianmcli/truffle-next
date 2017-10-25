5cfa9941dae44a731033465730c571f83956951b
Adrian Li <li.adrianmc@gmail.com>
2017-10-24 19:48:26 -0700

    hook up web3 and accounts to the next.js app

---
+++ client/lib/contractUtils.js
@@ -0,0 +1,6 @@
+export const getAccounts = web3 =>
+  new Promise((resolve, reject) => {
+    web3.eth.getAccounts(
+      (error, accounts) => (error ? reject(error) : resolve(accounts))
+    )
+  })
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import getWeb3 from '../lib/web3'
+import { getAccounts } from '../lib/contractUtils'
 
 export default class extends React.Component {
-  componentDidMount() {
-    // TODO - fetch web3 logic
+  state = { web3: null, accounts: null, contractInstance: null }
+
+  async componentDidMount() {
+    const web3 = await getWeb3()
+    const accounts = await getAccounts(web3)
+    console.log(web3, accounts)
   }
+
   render() {
     return (
       <div>Next.js is running properly.</div>
