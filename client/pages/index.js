a204a3e16f8eefb2198dcf41174b5f48c24cf012
Adrian Li <li.adrianmc@gmail.com>
2017-10-24 19:32:40 -0700

    new next.js project

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class extends React.Component {
+  componentDidMount() {
+    // TODO - fetch web3 logic
+  }
+  render() {
+    return (
+      <div>Next.js is running properly.</div>
+    )
+  }
+}
