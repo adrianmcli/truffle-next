5590224633afcc06c6d00d32ac6b9531ed8d0dfe
Adrian Li <li.adrianmc@gmail.com>
2017-10-28 03:26:31 -0700

    add example for calling a contract method

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,7 +3,7 @@ import getWeb3 from '../lib/web3'
 import { getAccounts, getContractInstance } from '../lib/utils'
 
 export default class extends React.Component {
-  state = { web3: null, accounts: null, contractInstance: null }
+  state = { web3: null, accounts: null, contractInstance: null, firstBalance: null }
 
   async componentDidMount() {
     try {
@@ -17,14 +17,27 @@ export default class extends React.Component {
     }
   }
 
+  getFirstAccountBalance = async () => {
+    const { accounts, contractInstance } = this.state
+    const response = await contractInstance.getBalance.call(accounts[0])
+    this.setState({ firstBalance: response.valueOf() })
+  }
+
   render() {
-    const { web3, accounts, contractInstance } = this.state;
+    const { web3, accounts, contractInstance } = this.state
     const appReady = web3 && accounts && contractInstance
+    // App is still loading
     if (!appReady) {
       return (<div>Loading web3, accounts, and contract instance.</div>)
     }
+    // App is ready
+    const { firstBalance } = this.state
     return (
-      <div>Ready!</div>
+      <div>
+        <h2>Ready!</h2>
+        <button onClick={this.getFirstAccountBalance}>Get first account balance</button>
+        <div>Balance: {firstBalance ? firstBalance : "N/A"}</div>
+      </div>
     )
   }
 }
