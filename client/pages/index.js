bf42eeae480452c56df732d91d07af4d00f9c1f8
Adrian Li <li.adrianmc@gmail.com>
2017-10-28 03:17:45 -0700

    add error handling for connection to blockchain and contract

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,10 +6,15 @@ export default class extends React.Component {
   state = { web3: null, accounts: null, contractInstance: null }
 
   async componentDidMount() {
-    const web3 = await getWeb3()
-    const accounts = await getAccounts(web3)
-    const contractInstance = await getContractInstance(web3)
-    this.setState({ web3, accounts, contractInstance })
+    try {
+      const web3 = await getWeb3()
+      const accounts = await getAccounts(web3)
+      const contractInstance = await getContractInstance(web3)
+      this.setState({ web3, accounts, contractInstance })
+    } catch (error) {
+      alert('Failed to load web3, accounts, and contract. Check console for details.')
+      console.log(error)
+    }
   }
 
   render() {
