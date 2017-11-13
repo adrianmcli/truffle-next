f5d18ca90250c4965a2179ff0ab004fbd3fbd266
Adrian Li <li.adrianmc@gmail.com>
2017-11-12 16:52:46 -0800

    remove old files

--- old/client/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  webpack: (config, { buildId, dev }) => {
-    config.resolve.symlinks = false
-    return config
-  }
-}
--- truffle.js
+++ truffle.js
@@ -1,4 +1,21 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    // Turns on the Solidity optimizer. For development the optimizer's
+    // quite helpful, just remember to be careful, and potentially turn it
+    // off, for live deployment and/or audit time. For more information,
+    // see the Truffle 4.0.0 release notes.
+    //
+    // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
 };
