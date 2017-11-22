177fc37b1e1cfe15660bf8478a63b872a4b7ad26
Adrian Li <li.adrianmc@gmail.com>
2017-11-22 00:36:02 -0800

    add comment

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   webpack: (config, { buildId, dev }) => {
+    // This allows the app to refer to files through our symlink
     config.resolve.symlinks = false
     return config
   }
