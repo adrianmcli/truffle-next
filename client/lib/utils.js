c9ce116fe1e9c999f1dcb080f47cd46157809f2a
Adrian Li <li.adrianmc@gmail.com>
2017-11-21 17:04:12 -0800

    upgrade web3 to 1.0.0

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -12,6 +12,16 @@ export const getContractInstance = async web3 => {
   const contract = initContract(contractDefinition)
   contract.setProvider(web3.currentProvider)
 
+  // Dirty hack for web3@1.0.0 support for localhost testrpc
+  // see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+  if (typeof contract.currentProvider.sendAsync !== "function") {
+    contract.currentProvider.sendAsync = function() {
+      return contract.currentProvider.send.apply(
+        contract.currentProvider, arguments
+      );
+    };
+  }
+
   const instance = await contract.deployed()
   return instance
 }
