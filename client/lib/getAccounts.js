bb358469db69ac019e250806931e28bfff805963
Adrian Li <li.adrianmc@gmail.com>
2018-07-09 17:23:59 -0400

    use native web3 api for getting accounts

--- client/lib/getAccounts.js
+++
@@ -1,9 +0,0 @@
-// Wrap web3.eth.getAccounts in a Promise
-const getAccounts = web3 =>
-  new Promise((resolve, reject) => {
-    web3.eth.getAccounts(
-      (error, accounts) => (error ? reject(error) : resolve(accounts))
-    )
-  })
-
-export default getAccounts
