966cf9cc7d44dbc19bf21fb17d950b3371bf570d
Prateek <prateek.panwar.2001@gmail.com>
2021-11-20 21:38:22 +0530

    updated the box to the latest version

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Migrations {
   address public owner;
@@ -8,7 +9,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  constructor() public {
+  constructor() {
     owner = msg.sender;
   }
 
---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      // Turns on the Solidity optimizer. For development the optimizer's
+      // quite helpful, just remember to be careful, and potentially turn it
+      // off, for live deployment and/or audit time. For more information,
+      // see the Truffle 4.0.0 release notes.
+      //
+      // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
+      version: '0.8.0',
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
